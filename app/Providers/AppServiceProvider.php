fdafdfc59a8566016eea436db78e54cafb6e6b71
Foo Bar <124519337+Ulduz23@users.noreply.github.com>
2023-09-12 11:03:22 +0400

    seo dinamik

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,8 @@ use App\Models\Comment;
 use App\Models\Contact;
 use App\Models\Posts;
 use App\Models\Settings;
+use App\Models\Seo;
+
 
 use App\Models\AdvertFooter;
 
@@ -42,6 +44,8 @@ class AppServiceProvider extends ServiceProvider
         View::share('last_messages',$last_messages);
         $setting = Settings::first();
         View::share('setting',$setting);
+        $seo = Seo::first();
+        View::share('seo',$seo);
         view()->composer('*', function($view) {
             $view->with('admin', auth()->user());
         });
