eb6daa8f4992c681d27722b6d0640c84581ad2e5
hac1yev <ilkinhaciyev955@gmail.com>
2023-09-15 15:47:39 +0400

    post add and post edit pages select2

--- resources/views/back/layouts/script.blade.php
+++ resources/views/back/layouts/script.blade.php
@@ -42,5 +42,7 @@
 
 <!-- CUSTOM JS -->
 <script src="{{asset('assets/js/custom.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
 @yield('js')
